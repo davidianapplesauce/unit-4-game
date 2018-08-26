fca28b8cebe89d98543872180d5bc7ab5e913ffe
David Applesauce <dsweck@gmail.com>
2018-08-25 23:07:18 -0400

    Hmwk 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,38 @@
 
 var targetNumber= "";
 var yourScore= 0;
-var images= ["assets/images/Aquamarine_.png", "assets/images/green_gem.png", "assets/images/Citrine_gem.png", "assets/images/purple_gem.png"];
+var crystals= ["assets/images/Aquamarine_.png", "assets/images/green_gem.png", "assets/images/Citrine_gem.png", "assets/images/purple_gem.png"];
 
 //functions
 
     function randomtargetNumber() {
         targetNumber= Math.floor(Math.random() * 50) + 21;
         
-   
+}
 
-};
+$("#randomNumber").text(randomtargetNumber);
+
+    function crystalClick() {
+
+    } 
+    
+    for (var i = 0; i <crystals.length; i++) {
+//not sure about this part
+        var imageCrystal = $("<img>");
+
+        imageCrystal.addClass("images");
+
+        imageCrystal.attr("crystals")
+
+        imageCrystal.attr("data-crystalvalue", crystals[i]);
+
+        $("#crystals").append(imageCrystal);
+
+    }
+    $(".images").on("click", function() {
+
+        var crystalvalue= ($(this).attr("data-crystalvalue"));
+        crystalvalue =parseInt(crystalvalue);
+
+        yourScore += crystalvalue;
+    })
