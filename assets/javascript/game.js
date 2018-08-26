cead6458667ca63b1023c54ca57389fe6e8fb63c
David Applesauce <dsweck@gmail.com>
2018-08-25 21:55:03 -0400

    Hmwk 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,14 @@
+//variables (no wins or losses yet)
 
+var targetNumber= "";
+var yourScore= 0;
+var images= ["assets/images/Aquamarine_.png", "assets/images/green_gem.png", "assets/images/Citrine_gem.png", "assets/images/purple_gem.png"];
+
+//functions
+
+    function randomtargetNumber() {
+        targetNumber= Math.floor(Math.random() * 50) + 21;
+        
+   
+
+};
