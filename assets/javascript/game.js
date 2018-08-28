d3c84ac861288bbbef70b38408676c3b395366bd
David Applesauce <dsweck@gmail.com>
2018-08-27 23:43:57 -0400

    Hmwk 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,35 +6,45 @@ var crystals= ["assets/images/Aquamarine_.png", "assets/images/green_gem.png", "
 
 //functions
 
-    function randomtargetNumber() {
-        targetNumber= Math.floor(Math.random() * 50) + 21;
+    
+targetNumber= Math.floor(Math.random() * 69) + 30; // random number generator
+$("#randomNumber").html (targetNumber);
         
+for(var i =0; i < 4; i++){
+    var random = Math.floor(Math.random() * 11) + 1;
+
+    $(".images").on('click', function(random) {
+        console.log($(this));
+    })
+     
 }
+
+
 
 //This(below) wasn't working:
 // $("#randomNumber").text(randomtargetNumber);
 
 //     function crystalClick() {
 
-    function resetCrystals () {
+//     function resetCrystals () {
     
-    for (var i = 0; i <crystals.length; i++) {
-//not sure about this part
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("images");
-        imageCrystal.attr("src", crystals[i])
-        crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
-        $(".images").append(imageCrystal);
+//     for (var i = 0; i <crystals.length; i++) {
+// //not sure about this part
+//         var imageCrystal = $("<img>");
+//         imageCrystal.addClass("images");
+//         imageCrystal.attr("src", crystals[i])
+//         crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+//         $(".images").append(imageCrystal);
 
-    }}
+//     }}
 
-    function resetHTML () {
-        $(".randomNumber").html(targetNumber);}
+//     function resetHTML () {
+//         $(".randomNumber").html(targetNumber);}
         
-    $(".images").on("click", function() {
+//     $(".images").on("click", function() {
 
-        var crystalvalue= ($(this).attr("data-crystalvalue"));
-        crystalvalue =parseInt(crystalvalue);
+//         var crystalvalue= ($(this).attr("data-crystalvalue"));
+//         crystalvalue =parseInt(crystalvalue);
 
-        yourScore += crystalvalue;
-    })
+//         yourScore += crystalvalue;
+//     })
